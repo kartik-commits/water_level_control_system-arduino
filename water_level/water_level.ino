e230bde044e06e2e97302c9ab85e6389576f9f09
kartik-commits <kartiklashkare@gmail.com>
2024-02-20 20:11:59 +0530

    water_level: Include float sensor and add conditional statements
    *Added 3rd float sensor to switch between auto mode and manual mode
    
    Signed-off-by: kartik-commits <kartiklashkare@gmail.com>

--- water_level/water_level.ino
+++ water_level/water_level.ino
@@ -7,6 +7,7 @@ LiquidCrystal lcd(10);
 const int relay = 0;
 const int floaty_1 = A4;
 const int floaty_2 = A3;
+const int floaty_3 = A2;
 const int led_1 = 6;
 const int led_2 = 7;
 const int led_3 = 4;
@@ -17,6 +18,7 @@ void setup() {
   pinMode(relay, OUTPUT);
   pinMode(floaty_1, INPUT);
   pinMode(floaty_2, INPUT);
+  pinMode(floaty_3, INPUT);
   pinMode(led_1, OUTPUT);
   pinMode(led_2, OUTPUT);
   pinMode(led_3, OUTPUT);
@@ -27,41 +29,47 @@ void setup() {
 }
 
 void loop() {
-  if((digitalRead(floaty_1) == LOW) && (digitalRead(floaty_2) == HIGH)) {
-    digitalWrite(relay, HIGH);
-    digitalWrite(led_1, HIGH);
-    digitalWrite(led_2, LOW);
-    digitalWrite(led_3, HIGH); 
-    lcd.setCursor(0, 0);
-    lcd.print("    PUMP ON     ");
-    delay(300);
+  if(digitalRead(floaty_3) == HIGH) {
+  
+    if((digitalRead(floaty_1) == LOW) && (digitalRead(floaty_2) == HIGH)) {
+      digitalWrite(relay, HIGH);
+      digitalWrite(led_1, HIGH);
+      digitalWrite(led_2, LOW);
+      digitalWrite(led_3, HIGH); 
+      lcd.setCursor(0, 0);
+      lcd.print("    PUMP ON     ");
+      delay(300);
+    }
+    else if(digitalRead(floaty_2) == LOW) {
+      digitalWrite(relay, LOW);
+      digitalWrite(led_3, LOW);
+      digitalWrite(led_2, HIGH);
+      lcd.setCursor(0, 0);
+      lcd.print("LOWER TANK EMPTY");
+      lcd.clear();
+      delay(300);
+      digitalWrite(led_3, HIGH);
+      lcd.setCursor(0, 0);
+      lcd.print("LOWER TANK EMPTY");
+      delay(300);
+    }
+    else if(digitalRead(floaty_1) == HIGH) {
+      digitalWrite(relay, LOW);
+      digitalWrite(led_1, HIGH);
+      digitalWrite(led_2, HIGH);
+      digitalWrite(led_3, HIGH);
+      lcd.setCursor(0, 0);
+      lcd.print("    TANK FULL   ");
+      lcd.clear();
+      delay(300);
+      lcd.setCursor(0, 0);
+      lcd.print("    TANK FULL   ");
+      digitalWrite(led_1, LOW);
+      delay(300);
+    }
   }
-  else if(digitalRead(floaty_2) == LOW) {
-    digitalWrite(relay, LOW);
-    digitalWrite(led_3, LOW);
-    digitalWrite(led_2, HIGH);
-    lcd.setCursor(0, 0);
-    lcd.print("LOWER TANK EMPTY");
-    lcd.clear();
-    delay(300);
-    digitalWrite(led_3, HIGH);
-    lcd.setCursor(0, 0);
-    lcd.print("LOWER TANK EMPTY");
-    delay(300);
+  else {
+    //TODO
   }
-  else if(digitalRead(floaty_1) == HIGH) {
-    digitalWrite(relay, LOW);
-    digitalWrite(led_1, HIGH);
-    digitalWrite(led_2, HIGH);
-    digitalWrite(led_3, HIGH);
-    lcd.setCursor(0, 0);
-    lcd.print("    TANK FULL   ");
-    lcd.clear();
-    delay(300);
-    lcd.setCursor(0, 0);
-    lcd.print("    TANK FULL   ");
-    digitalWrite(led_1, LOW);
-    delay(300);
-  }
-}
 
+}
